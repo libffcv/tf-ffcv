a7097910c539da24416e4b300268361f1423af9e
Guillaume Leclerc <leclerc@mit.edu>
2022-01-20 03:54:09 -0500

    Bump version to release on PIP with the readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="tf-ffcv",
-    version="0.0.1",
+    version="0.0.2",
     packages=["tf_ffcv"],
     description='Utilitaries to integrate tensorflow to FFCV',
     author='MadryLab',
--- tf_ffcv/__init__.py
+++ tf_ffcv/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
